2356ed30201974881e09b1452ae014ed66627c13
terryhale <aggiefan17@yahoo.com>
2017-11-15 16:01:44 -0600

    added functionality to adding movies and delete button

--- src/index.js
+++ src/index.js
@@ -11,88 +11,97 @@ sayHello(`Terry`);
 const {getMovies} = require('./api.js');
 const $ = require ('jquery');
 
-$('#submit-button').click(function (e) {
-    e.preventDefault();
-    let title = $('#movie-title').val();
-    let rating = $('#movie-ratings').val();
-    let movie = {
-        title: title,
-        rating: rating
-    };
-    console.log(movie);
-    fetch('/api/movies', {
-        headers: {"content-type": "application/json"},
-        method: "POST",
-        body: JSON.stringify({title, rating})
-    }).then(() => {
-        return getMovies();
-    }).then(movies => {
-        $(".bodyHTML").append(movies);
-    });
-});
 
 
 
 
-getMovies().then(movies => {
 
-    let table =
-        // "<h2>Movie Catalog</h2>" +
-        "<table class='tbl-heading'>"+
-          "<thead>" +
-              "<tr>" +
-                  "<th width='200' class='title-head'>Title</th>" +
-                  "<th width='200' class='id-head'>ID</th>" +
-                  "<th width='200' class='rating-head'>Rating</th>" +
-                  "<th width='57' class='delete'>Delete</th>" +
-              "</tr>" +
-          "</thead>"+
-        "</table>";
+function updateMovies() {
 
-    movies.forEach(function (movie) {
+    getMovies().then(movies => {
 
-        let title = movie.title;
-        let id = movie.id;
-        let rating = movie.rating;
+        let table =
+            // "<h2>Movie Catalog</h2>" +
+            "<table class='tbl-heading' id='movie-table'>"+
+            "<thead>" +
+            "<tr>" +
+            "<th width='200' class='title-head'>Title</th>" +
+            "<th width='200' class='rating-head'>Rating</th>" +
+            "<th width='57' class='delete'>Delete</th>" +
+            "</tr>" +
+            "</thead>"+
+            "<tbody>";
 
-        table +=
-         `<table>
-            <tbody>
-                <tr>
+        movies.forEach(function (movie) {
+
+            let title = movie.title;
+            let id = movie.id
+            let rating = movie.rating;
+
+            table +=
+                `<tr>
                     <td width="200" class="title-block">${title}</td>
-                    <td width="200">${id}</td>
                     <td width="200">${rating}</td>
-                    <td><button class="delete-btn" >Delete</button></td>
-                </tr>
-            </tbody>
-          </table>`;
-    });
+                    <td><button data-id="${id}" class="delete-btn clear">Delete</button></td>
+                </tr>`;
+        });
 
-    $(".bodyHTML").html(table);
+        table += `</tbody></table>`;
 
-    $('.delete-btn').click(function (e) {
+        $(".bodyHTML").html(table);
+
+        $('.clear').click(function (e) {
         e.preventDefault();
+        console.log(e.target);
+        let id = $(this).attr("data-id");
         console.log("clicked");
-         fetch(`/api/movies`, {
+         fetch(`/api/movies/${id}`, {
              headers: {"content-type": "application/json"},
              method: "DELETE"
          }).then(() => {
              getMovies().then((movies) => {
             console.log(movies);
+            updateMovies();
              });
         });
+        });
+
+        console.log('Here are all the movies:');
+        console.log(movies);
+
+        movies.forEach(({title, rating, id}) => {
+            console.log(`${id} ${title} ${rating}`);
+        });
+
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
     });
 
-    console.log('Here are all the movies:');
-    console.log(movies);
+};
 
-    movies.forEach(({title, rating, id}) => {
-      console.log(`${id} ${title} ${rating}`);
+updateMovies();
+
+
+    $('#submit-button').click(function (e) {
+        e.preventDefault();
+        let title = $('#movie-title').val();
+        let rating = $('#movie-ratings').val();
+        let movie = {
+            title: title,
+            rating: rating
+        };
+        console.log(movie);
+        fetch('/api/movies', {
+            headers: {"content-type": "application/json"},
+            method: "POST",
+            body: JSON.stringify({title, rating})
+        }).then(() => {
+            return getMovies();
+        }).then((movies) => {
+            console.log(movies);
+            updateMovies();
+        });
     });
-
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
-});
 
 
